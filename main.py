aa689d784b43b7a1fc8b39f0042f65266dd1832e
Michael-Zapivahin <zapivahin@ya.ru>
2023-06-07 10:50:19 +0300

    random flash

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ import curses
 import asyncio
 
 
-async def blink(canvas, row, column, symbol='*'):
+async def blink(canvas, row, column, symbol='*', delay=[]):
+    for time in delay:
+        await asyncio.sleep(0)
+
     while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        for counter in range(1, 11, 1):
+        canvas.addstr(row, column, f'{symbol}-1', curses.A_DIM)
+        for counter in range(1, 11):
             await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        for counter in range(1, 3, 1):
+        canvas.addstr(row, column, f'{symbol}-2')
+        for counter in range(1, 3):
             await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for counter in range(1, 5, 1):
+        canvas.addstr(row, column, f'{symbol}-3', curses.A_BOLD)
+        for counter in range(1, 5):
             await asyncio.sleep(0)
 
-        canvas.addstr(row, column, symbol)
-        for counter in range(1, 3, 1):
+        canvas.addstr(row, column, f'{symbol}-4')
+        for counter in range(1, 3):
             await asyncio.sleep(0)
 
 
@@ -29,8 +32,11 @@ def draw(canvas):
     for column in range(1, 10):
         for row in range(1, 5):
             coroutines.append(blink(
-                canvas, random.randint(1, 7),
-                random.randint(1, 150), random.choice(symbols)
+                canvas,
+                random.randint(1, 7),
+                random.randint(1, 150),
+                random.choice(symbols),
+                range(1, random.randint(1, 20))
             ))
 
     canvas.border()
@@ -39,7 +45,7 @@ def draw(canvas):
             try:
                 coroutine.send(None)
                 canvas.refresh()
-                time.sleep(1/100)
+                time.sleep(1/200)
             except StopIteration:
                 break
 
