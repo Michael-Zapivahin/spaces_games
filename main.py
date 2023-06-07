d2e430a1ca9f635d252771fd47d19a3f1ab96515
Michael-Zapivahin <zapivahin@ya.ru>
2023-06-07 09:05:08 +0300

    made flash

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ import curses
 
 def draw(canvas):
     row, column = (5, 20)
-    canvas.border()
-    flash = True
     while True:
-        if flash:
-            canvas.addstr(row, column, '*')
-            flash = False
-        else:
-            canvas.addstr(row, column, ' ')
-            flash = True
+        canvas.clear()
+        canvas.border()
+        canvas.addstr(row, column, '*', curses.A_DIM)
         canvas.refresh()
-        time.sleep(1/10)
+        time.sleep(2)
+        canvas.addstr(row, column, '1')
+        canvas.refresh()
+        time.sleep(3/10)
+        canvas.addstr(row, column, '2', curses.A_BOLD)
+        canvas.refresh()
+        time.sleep(5/10)
+        canvas.addstr(row, column, '3')
+        canvas.refresh()
+        time.sleep(3/10)
 
 
 if __name__ == '__main__':
     window = curses.initscr()
     curses.curs_set(0)
     curses.update_lines_cols()
+    curses.A_DIM
     curses.wrapper(draw)
